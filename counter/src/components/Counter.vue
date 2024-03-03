648b10ca033941e2275be2e7c14537e5a70244de
cryswerton <cryswertons@gmail.com>
2024-03-03 17:02:49 +0000

    added counter project

---
+++ counter/src/components/Counter.vue
@@ -0,0 +1,16 @@
+<template>
+    <button @click="count++">
+    Count is: {{ count }}
+  </button>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+let count = ref(0)
+
+</script>
+
+<style scoped>
+    
+</style>
